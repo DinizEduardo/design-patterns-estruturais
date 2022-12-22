aa9fe6040f8533da859ca061d406c06634e0a0fe
Eduardo Diniz <dinizdudi@gmail.com>
2022-12-22 11:01:03 -0300

    feat: aplicando state pattern para desconto extra

--- src/main/java/br/com/alura/loja/orcamento/Orcamento.java
+++ src/main/java/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,15 +1,38 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
     private Integer qtdItens;
+    private SituacaoOrcamento situacao;
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
 
     public Orcamento(BigDecimal valor, Integer qtdItens) {
         this.valor = valor;
         this.qtdItens = qtdItens;
+        this.situacao = new EmAnalise();
     }
 
     public Integer getQtdItens() {
@@ -18,6 +41,14 @@ public class Orcamento {
 
     public BigDecimal getValor() {
         return valor;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 
 }
